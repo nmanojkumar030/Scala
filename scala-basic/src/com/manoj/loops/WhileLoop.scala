fb84eea0583cadf18e7eafa24b24f39b20882995
mnanjundegowda <nmanojkumar030@gmail.com>
2018-10-28 09:21:22 +0530

    Adding loops

---
+++ scala-basic/src/com/manoj/loops/WhileLoop.scala
@@ -0,0 +1,20 @@
+package com.manoj.loops
+
+object WhileLoop {
+  def main(args: Array[String]): Unit = {
+
+    // While Loop
+    var x = 10
+    while (x > 0) {
+      println(x)
+      x -= 1
+    }
+
+    println("*" * 100)
+    var y = 0
+    do {
+      println(y)
+      y += 1
+    } while (y < 10)
+  }
+}
